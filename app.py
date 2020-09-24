b9edbd85fcaa33d29173f1b6167656a7a05e9943
Rachithooda <70371526+Rachithooda@users.noreply.github.com>
2020-09-24 13:51:00 +0530

    Added a Type seaction and /admin

--- app.py
+++ app.py
@@ -14,12 +14,19 @@ class Project(db.Model):
     author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
+    cat = db.Column(db.String(20))
 
 @app.route('/')
 def index():
     posts = Project.query.order_by(Project.date_posted.desc()).all()
 
     return render_template('index.html', posts=posts)
+
+@app.route('/admin')
+def admin():
+    posts = Project.query.order_by(Project.date_posted.desc()).all()
+
+    return render_template("admin_index.html", posts=posts)
 
 @app.route('/search')
 def search():
@@ -40,13 +47,21 @@ def addpost():
     title = request.form['title']
     author = request.form['author']
     content = request.form['content']
+    cat = request.form['cat']
 
-    post = Project(title=title, author=author, content=content, date_posted=datetime.now())
+    post = Project(title=title, author=author, content=content, cat=cat, date_posted=datetime.now())
 
     db.session.add(post)
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    post = Project.query.filter_by(id=post_id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     db.create_all()
